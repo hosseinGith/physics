29361b1ce920a309458563b4612d82d32eb61c37
hosseinGithub <133234861+hosseinGith@users.noreply.github.com>
2023-11-10 23:10:56 +0330

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -100,7 +100,7 @@ function animateTouch() {
   } else if (car.x >= canvas.width - car.width) {
     car.varSpeed += -Math.abs(car.varSpeed) * 1.3;
   }
-  if (car.y >= canvas.height) {
+  if (car.y >= canvas.height - car.height - 10) {
     car.y = 0;
   }
   drawCar();
